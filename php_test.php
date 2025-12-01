5fb8b6c5bba5d18aa71c9f2485daa90589bdc2ee
Fahad <fahadbinhussain001@gmail.com>
2025-12-01 21:34:08 +0600

    Remove deprecated files: Delete old appointment management scripts, configuration examples, and database setup SQL for cleanup and refactoring.

--- php_test.php
+++
@@ -1,6 +0,0 @@
-<?php
-// Basic PHP test for InfinityFree
-header('Content-Type: application/json');
-
-echo '{"status": "PHP is working", "time": "' . date('Y-m-d H:i:s') . '", "post_data": ' . json_encode($_POST) . '}';
-?>
